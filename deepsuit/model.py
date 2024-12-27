c1460eac61f6d6c00a668df130c01a9a807536a5
zhengkai15 <156252108@qq.com>
2024-12-27 19:42:39 +0800

    update model

--- deepsuit/model.py
+++ deepsuit/model.py
@@ -7,10 +7,11 @@ def get_model(config):
     if config["model"]["name"] == "unet":
         from .models.unet import Model
         best_model_path = config["exp"]["best_model_path"]
-        in_varibale_num = 24
-        in_times_num = 72
-        out_varibale_num = 1
-        out_time_num = 72
+        best_model_path = config["exp"]["best_model_path"]
+        in_varibale_num = config["model"]["in_varibale_num"]
+        in_times_num = config["model"]["in_times_num"]
+        out_varibale_num = config["model"]["out_varibale_num"]
+        out_time_num = config["model"]["out_time_num"]
         input_size = in_times_num * in_varibale_num
         output_size = out_time_num * out_varibale_num
         params = {'in_channels': input_size,
@@ -18,7 +19,22 @@ def get_model(config):
         
         model = Model(params=params)
         if best_model_path is not None:
-            model.load_state_dict(torch.load(best_model_path))      
+            model.load_state_dict(torch.load(best_model_path))  
+    elif config["model"]["name"] == "unet_yx_nowcasting":
+        from .models.unet_yx_nowcasting import Model
+        best_model_path = config["exp"]["best_model_path"]
+        in_varibale_num = config["model"]["in_varibale_num"]
+        in_times_num = config["model"]["in_times_num"]
+        out_varibale_num = config["model"]["out_varibale_num"]
+        out_time_num = config["model"]["out_time_num"]
+        input_size = in_times_num * in_varibale_num
+        output_size = out_time_num * out_varibale_num
+        params = {'in_channels': input_size,
+                'out_channels': output_size}
+        model = Model(params=params)
+        if best_model_path is not None:
+            model.load_state_dict(torch.load(best_model_path))  
+                
     elif config["model"]["name"] == "temporal_model":
         from models.unet import Model
         # model = TemporalModel(ModelConfig(), configs=config)
